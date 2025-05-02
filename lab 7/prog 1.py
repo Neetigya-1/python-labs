2685389ce74d3f70c2fe0e089bf00f5e2cd43149
Neetigya-1 <neetigya2006@gmail.com>
2025-05-02 09:54:19 +0530

    Add files via upload

---
+++ lab 7/prog 1.py
@@ -0,0 +1,7 @@
+print("Neetigya,24BEE113")
+list =["hello" ,"hi", "bye" ,"please"]
+set =set()
+for i in list:
+    set.add(i.upper())
+   
+print(f"The set in uppercase is {set}")
---
+++ lab 7/prog 2.py
@@ -0,0 +1,14 @@
+print("Neetigya,24bee113")
+import random
+randomset = {random.randrange (15,45) for _ in range (10)}
+print(set)
+removeset= set()
+print(randomset)
+c=0
+for i in randomset :
+    if(i<30):
+        c+=1
+    else:
+        removeset.add(i)
+randomset-=removeset
+print(randomset)
